9ec747bbc88686d84008484be8f64e91c92d5b12
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-08-14 16:13:54 +0100

    gt

--- server-app/src/config/redisClient.ts
+++ server-app/src/config/redisClient.ts
@@ -1,12 +1,27 @@
 import { createClient } from 'redis';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Determine which Redis URL or configuration to use
+const isProduction = process.env.NODE_ENV === 'production';
 
 const redisClient = createClient({
-    password: process.env.REDIS_PASSWORD,
-		socket: {
+	url: isProduction ? undefined : process.env.REDIS_URL, //use REDIS_URL for non-production environment
+    password: isProduction ? process.env.REDIS_PASSWORD : undefined,
+		socket : isProduction ? {
 			host: process.env.REDIS_HOST,
 			port: parseInt(process.env.REDIS_PORT as string),
-		},
+		} : undefined,
 });
+
+// Log Redis connection details based on environment
+if (!isProduction) {
+	console.log('Local Redis client configured');
+  } else {
+	console.log('Production Redis client configured.');
+  }
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
